4357f4b486db691f6456422ff765da3c10fcfd9c
Zemorath <trentward10@gmail.com>
2023-08-11 22:53:29 -0500

    Event Handling Lab Complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe () {
+    function blurChange(event) {
+        console.log('Hey! Eyes on me!')
+    }
+
+    function focusChange(event) {
+        console.log('Good!')
+    }
+
+    return (
+        <button onBlur={blurChange} onFocus={focusChange}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
